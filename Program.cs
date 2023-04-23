3247a7c8ac639de4fe89ffc46c08c3e2c66c1532
Kate <katyrakovskaya@gmail.com>
2023-04-23 15:57:40 +0700

    Написана программа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Compare(string[] words, string[] result){
+    for (int i = 0; i < words.Length; i++){
+        if (words[i].Length > 3) continue;
+        else result[i] = words[i];
+    }
+    for (int j = 0; j < result.Length; j++) Console.Write($"{result[j]} ");
+}
+
+Console.WriteLine("Введите массив строк: ");
+string[] words = Console.ReadLine().Split(" ").ToArray();
+string[] result = new string[words.Length];
+Console.WriteLine("Строки с 3 или меньшим количеством символов: ");
+Compare(words, result);
+
